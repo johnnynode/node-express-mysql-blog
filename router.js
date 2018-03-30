53d9b0cdb332319c37e1a48dd460b05b669a86cd
johnnynode <johnnynode@gmail.com>
2018-03-30 17:48:42 +0800

    add login function and page

--- router.js
+++ router.js
@@ -13,6 +13,9 @@ router.get('/page/:pageNumber',articleCtrl.getByPageNumber);
 router.get('/register', [checkLogin, userCtrl.showRegister]); // 获取注册页面
 router.post('/register', userCtrl.doRegister); // 用户注册
 
+router.get('/login', [checkLogin, userCtrl.showLogin]); // 获取登录页面
+router.post('/login', userCtrl.doLogin); // 用户登录
+
 
 function checkLogin(req, res, next) {
   // 权限校验，已经登录的用户，就不要再访问这个页面了，直接跳转到首页就行了
